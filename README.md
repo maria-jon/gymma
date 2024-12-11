ec524b82757098fd6642fde74554fbcf909e156d
maria-jon <mariajonasson93@gmail.com>
2024-12-11 20:05:13 +0100

    add styling

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Gymma?
+
+## Description
+
+A simple yes/no answer if you should go to the gym. Chances are 80/20 for yes/no. 
--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function getYesOrNo() {
     // Generate a random number between 0 and 1
     const randomNumber = Math.random();
     // Return "Yes" 80% of the time and "No" 20% of the time
-    return randomNumber < 0.8 ? 'Ja' : 'Nej';
+    return randomNumber < 0.8 ? `<h2 class="yes">Ja.</h2> <p>Och refresha <em>inte</em> sidan för att försöka komma undan!</p>` : `<h2 class="no">Nej.</h2> <p>Lyllo dig!</p>`;
 }
 
 // Display the answer on the page
-document.getElementById('answer').textContent = getYesOrNo();
+document.getElementById('answer').innerHTML = getYesOrNo();
